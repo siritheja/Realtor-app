3e0094523046848ed208c299743129cba790b944
Ponugoti Siri Theja <psiritheja@gmail.com>
2021-02-07 18:09:55 +0530

    initial commit

--- btre/settings.py
+++ btre/settings.py
@@ -43,7 +43,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'btre.urls'
 
